5bebee0c5315db5de12644e3426d9ea89f97d296
IkramHazim280721 <81061150+Hazim280721@users.noreply.github.com>
2021-05-04 22:35:32 +0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,30 @@
+
+
+public class main {
+
+	public static void main(String[] args) {
+		Fruit b=new Fruit("Fruit");
+		
+        System.out.println(b);
+        System.out.println();
+        
+		Apple a=new Apple("Apple","September-November",2,4);
+		
+		System.out.println(a);
+		System.out.println();
+        
+        redApple c=new redApple("Red Apple","October",4,5,"Red");
+        
+        System.out.println(c);
+        System.out.println();
+        
+        greenApple d=new greenApple("Green Apple","November",7,4,"Green");
+       
+        System.out.println(d);
+        
+	}
+
+}
+
+
+	
